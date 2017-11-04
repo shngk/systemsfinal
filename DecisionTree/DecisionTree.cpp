26d18a65470d9b225a89eeba72fc5875b68e106c
邵寒 <hshao@ip-103-196.wireless.oberlin.edu>
2017-11-04 15:42:19 -0400

    print

--- DecisionTree/DecisionTree.cpp
+++ DecisionTree/DecisionTree.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ifstream dataset;
+    dataset.open("../iris.data");
+    char output[100];
+    if (dataset.is_open()) {
+    while (!dataset.eof()) {
+
+
+       dataset >> output;
+       cout<< output << endl;
+
+
+    }
+   }
+   else{
+        cout << "fail" << endl;
+   }
+   dataset.close();
+   return 0;
+
+
 }
