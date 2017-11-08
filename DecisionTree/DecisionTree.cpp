1fedf379e1ab31c3b92e68e0bc846de6ec60d68f
邵寒 <hshao@ip-103-196.wireless.oberlin.edu>
2017-11-08 14:22:45 -0500

    vector

--- DecisionTree/DecisionTree.cpp
+++ DecisionTree/DecisionTree.cpp
@@ -2,7 +2,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-
+#include <map>
+#include <array>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -10,15 +15,29 @@ int main()
     ifstream dataset;
     dataset.open("../iris.data");
     char output[100];
+    string line;
     if (dataset.is_open()) {
-    while (!dataset.eof()) {
+        while (!dataset.eof()) {
+            map<string, vector<double>> data;
+            vector<double> each;
+            getline(dataset, line);
+            size_t pos = 0;
+            string token;
+            string delimiter = ",";
+
+            while((pos = line.find(delimiter)) != string::npos){
+                token = line.substr(0,pos);
+                each.push_back(stod(token, NULL));
+                line.erase(0,pos+delimiter.length());
+            }
+            for(int i = 0; i <each.size(); i++){
+                cout << each[i] << " ";
+            }
+            cout << token ;
+            cout << endl;
 
 
-       dataset >> output;
-       cout<< output << endl;
-
-
-    }
+        }
    }
    else{
         cout << "fail" << endl;
