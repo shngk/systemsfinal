581cb9294617c7c137bfec0f8ecc070d07194c3f
Rebecca Lawrence <rlawrenc@clyde>
2017-11-28 17:58:12 -0500

    fixed the seg-fault from assign_pts(), but now there's a problem calculating the distances...

--- kmeans/kfunctions.cpp
+++ kmeans/kfunctions.cpp
@@ -34,35 +34,41 @@ centroid* get_random_centroid(){
 
   return cent;
 }
-
+/*DEBUG: well, this lil bitch prints -nan after the first run through the function
+instead of the distance it's supposed to calculate.  */
 void assign_pts(vector<vector<string> > data, centroid *c1, centroid *c2, centroid *c3){
   // for each item in data, calculate its distance from each centroid and
   // add that item to the vector contained in the centroid it is closest to
   unsigned int i;
 
-  for(i = 0; i < data.size(); i++){
+  for(i = 0; i < data.size() - 1; i++){
     double d1;
     double d2;
     double d3;
     
     // calculate distance from each centroid
-    printf()
+    printf("data[%d][2] = %s, data[%d][3] = %s\n", i, data[i][2].c_str(), i, data[i][3].c_str());  //DEBUG
+
+
     double len = atof(data[i][2].c_str());
-    double wid = atof(data[i][2].c_str());
+    double wid = atof(data[i][3].c_str());
 
     d1 = sqrt(pow((len - c1->x), 2) + pow((wid - c1->y), 2));
     d2 = sqrt(pow((len - c2->x), 2) + pow((wid - c2->y), 2));
     d3 = sqrt(pow((len - c3->x), 2) + pow((wid - c3->y), 2));
-
+    printf("calculated lengths: %f, %f, %f\n", d1, d2, d3); //DEBUG
     // check which distance has the smallest value
     // add the item to the appropriate centroid
     if (d1 <= d2 && d1 <= d3){
+      cout << "added to d1\n";
       c1->elements.push_back(data[i]);
     }
     else if(d2 <= d1 && d2 <= d3){
+      cout << "added to d2\n";
       c2->elements.push_back(data[i]);
     }
     else if(d3 <= d1 && d3 <= d2){
+      cout << "added to d3\n";
       c3->elements.push_back(data[i]);
     }
   }
