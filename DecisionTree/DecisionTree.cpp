3fb4a52526eddd3b7326e82d69e73cd0744f083a
邵寒 <hshao@ip-103-196.wireless.oberlin.edu>
2017-12-03 15:10:58 -0500

    add num_type

--- DecisionTree/DecisionTree.cpp
+++ DecisionTree/DecisionTree.cpp
@@ -11,12 +11,13 @@
 
 using namespace std;
 
-struct each_row{
+struct entry{
     string type;
+    int num_type;
     vector<double> attributes;
 };
 
-void read_data(ifstream &dataset, vector<each_row> &data);
+void read_data(ifstream &dataset, vector<entry> &data);
 
 
 
@@ -26,12 +27,12 @@ int main()
     dataset.open("../iris.data");
 
     if (dataset.is_open()) {
-       vector<each_row> data;
+       vector<entry> data;
        read_data(dataset, data);
-       /*for(int i = 0; i < data.size(); i++){
-           cout << data[i].attributes[0] << " "<< data[i].attributes[1] << " " << data[i].attributes[2] ;
-           cout << data[i].type << endl;
-       }*/
+       for(int i = 0; i < (int)data.size(); i++){
+           cout << data[i].attributes[0] << " "<< data[i].attributes[1] << " " << data[i].attributes[2] << " " ;
+           cout << data[i].num_type << " "<< data[i].type << endl;
+       }
     }
     else{
          cout << "fail" << endl;
@@ -42,18 +43,19 @@ int main()
 
 }
 
-void read_data(ifstream &dataset, vector<each_row> &data){
+void read_data(ifstream &dataset, vector<entry> &data){
 
 
-
+    int num = 0;
     while (!dataset.eof()) {
+
         string line;
         string token;
         string delimiter = ",";
         getline(dataset, line);
         size_t pos = 0;
 
-         each_row new_row;
+         entry new_row;
          while((pos = line.find(delimiter)) != string::npos){
               token = line.substr(0,pos);
               double d = stod(token, NULL);
@@ -61,7 +63,14 @@ void read_data(ifstream &dataset, vector<each_row> &data){
               line.erase(0,pos+delimiter.length());
 
          }
+
+
+
+         if(data.size() != 0 && data[data.size()-1].type != line){
+             num++;
+         }
          new_row.type = line;
+         new_row.num_type = num;
          data.push_back(new_row);
     }
 }
