0ebbce82d37d6d0c805a7f3d901516996ee94b3b
Han Shao <hshao@ip-103-196.wireless.oberlin.edu>
2017-12-09 15:23:18 -0500

    create attributes vector

--- DisplayTree/DecisionTree.cpp
+++ DisplayTree/DecisionTree.cpp
@@ -57,6 +57,7 @@ int main(int argc, char *argv[]){
     ifstream dataset;
     dataset.open("../DecisionTree/iris.data");
     if (dataset.is_open()) {
+       vector<string> names = {"Sepal Length", "Sepal Width", "Petal Length", "Petal Width"};
        vector<entry*> data;
        read_data(dataset, data);
        node* root = new node;
